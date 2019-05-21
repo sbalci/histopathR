71f8f1daea6e809e54ead8cec8867cfb74b7f823
sbalci <drserdarbalci@gmail.com>
2019-05-21 23:05:25 +0300

    remove example codes

--- exampleR/cleanSurgPathNo.R
+++
@@ -1,7 +0,0 @@
-sarcomatoid$surgpathno <- trimws(sarcomatoid$surgpathno)
-
-sarcomatoid$surgpathno <- gsub(pattern = "[a-zA-Z]", replacement = "", x = sarcomatoid$surgpathno)
-
-sarcomatoid$surgpathno <- gsub(pattern = "-", replacement = " ", x = sarcomatoid$surgpathno)
-
-sarcomatoid$surgpathno <- substr(x = sarcomatoid$surgpathno, start = 1, stop = 8)
